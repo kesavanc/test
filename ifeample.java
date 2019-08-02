6b0ac15191bf8b4324253b4a84d82cc388ccb3cf
kesavanc <53558545+kesavanc@users.noreply.github.com>
2019-08-02 18:18:47 +0530

    Add files via upload

---
+++ ifeample.java
@@ -0,0 +1,20 @@
+package day2.examples;
+
+public class ifeample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=10;
+		int y=20;
+		if(x<y) {
+			System.out.println("x is lessthan y");
+		}
+		if (x==y) {
+			System.out.println("x is equal to y");
+		}
+		else if(x>y){
+			System.out.println("x is greater than y");
+		}
+	}
+
+}
