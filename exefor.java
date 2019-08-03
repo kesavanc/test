d032519f5d4c2f5da3d94de51f85da60defa0543
kesavanc <53558545+kesavanc@users.noreply.github.com>
2019-08-03 11:52:47 +0530

    program added it

--- exefor.java
+++ exefor.java
@@ -4,3 +4,15 @@ welocme to new world
 for syntax 
 check condition
 compile & execution
+package day3.examples;
+
+public class exefor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=10; i >0 ;i--)
+			System.out.println("i="+i);
+
+	}
+
+}
